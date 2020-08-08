c7e7ee55a5b4f2419ada2776d770507149a4a410
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-07 22:06:28 -0400

    user rendered

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import UserCard from "./components/UserCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    console.log("CDM");
+    axios
+      .get("https://api.github.com/users/Patrick-G-Oliver")
+      .then(res => {
+        console.log(res.data)
+        this.setState({ user: res.data })
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <UserCard user={this.state.user} />
+      </div>
+    );
+  }
 }
 
 export default App;
